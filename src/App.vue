8143988a084623c7295118b6ea5dcee60863f720
vladpliuta <vlad.pliuta@gmail.com>
2017-07-08 15:22:37 +0300

    com1

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <menu-log-in></menu-log-in>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import Menu from './components/menu.vue'
+export default {
+  name: 'app',
+  components: {
+      'menu-log-in': Menu
+  }
+}
+</script>
---
+++ src/components/menu.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav class="navbar ">
+    <div class="navbar-menu">
+      <div class="navbar-end">
+        <a class="button is-primary">Log in</a>
+      </div>
+    </div>
+  </nav>
+
+</template>
+
+<script>
+
+</script>
